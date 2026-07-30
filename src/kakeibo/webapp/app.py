c796e11b80cb798c87153a3eb61784a23d529605
Tateishi Tadatoshi <ishio39@gmail.com>
2026-07-30 23:51:14 +0900

    仕訳一覧表示機能追加

--- src/kakeibo/webapp/app.py
+++ src/kakeibo/webapp/app.py
@@ -1,11 +1,17 @@
 import streamlit as st
 
+from kakeibo.services import ledger
 
 def app():
     title = "Hello World!"
     st.set_page_config(layout="wide", page_title=title)
 
     st.header(title)
+
+    st.session_state.raw_data = ledger.read()
+
+    st.dataframe(st.session_state.raw_data)
+
 
 if __name__=="__main__":
     app()
