e55cf6c45952cdeda1f831adecbea1e841090dbc
Tateishi Tadatoshi <ishio39@gmail.com>
2026-08-05 23:56:57 +0900

    cli.py の修正

--- src/kakeibo/cli.py
+++ src/kakeibo/cli.py
@@ -1,19 +1,21 @@
-import sys
 import subprocess
+import sys
+from pathlib import Path
 
-from .services import ledger
+from kakeibo import services
 
 
 def app():
     try:
-        df = ledger.read()
+        file = Path("~/wks/ledger/ledger_kakei/journal/kakei/main.ledger")
+        df = services.read_ledger(file)
         for row in df.itertuples():
             print(row)
-        # print(df)
     except RuntimeError as e:
         print(f"RuntimeError: {e}", file=sys.stderr)
     except subprocess.CalledProcessError as e:
         print(f"CalledProcessError: {e}, {e.stderr}", file=sys.stderr)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     app()
