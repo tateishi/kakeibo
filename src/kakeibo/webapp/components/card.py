c54f3ab4d7fb24875f8fd7bddf6c429d660402d6
Tateishi Tadatoshi <ishio39@gmail.com>
2026-08-14 00:48:26 +0900

    card コンポーネントを中央揃えにする

--- src/kakeibo/webapp/components/card.py
+++ src/kakeibo/webapp/components/card.py
@@ -9,6 +9,7 @@ def card(title: str, contents: str, bgcolor: str="#222222"):
     padding:20px;
     margin: 5px 0px;
     box-shadow: 2px 2px 8px rgba(0,0,0,0.1);
+    text-align: center;
 ">
     <h3 style="margin:0;">{title}</h3>
     <p style="font-size:1.4rem; font-weight:600;">{contents}</p>
