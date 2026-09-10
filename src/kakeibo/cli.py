d9f8fa865fd5056668441d152260bafff0c0f9f3
Tateishi Tadatoshi <ishio39@gmail.com>
2026-09-10 22:49:28 +0900

    typer の利用を始める

--- src/kakeibo/cli.py
+++ src/kakeibo/cli.py
@@ -1,21 +1,10 @@
-import subprocess
-import sys
-from pathlib import Path
+import typer
 
-from kakeibo import services
+from kakeibo import commands
 
+app = typer.Typer()
 
-def app():
-    try:
-        file = Path("~/wks/ledger/ledger_kakei/journal/kakei/main.ledger")
-        df = services.read_ledger(file)
-        for row in df.itertuples():
-            print(row)
-    except RuntimeError as e:
-        print(f"RuntimeError: {e}", file=sys.stderr)
-    except subprocess.CalledProcessError as e:
-        print(f"CalledProcessError: {e}, {e.stderr}", file=sys.stderr)
-
+app.add_typer(commands.app)
 
 if __name__ == "__main__":
     app()
---
+++ src/kakeibo/commands/dump.py
@@ -0,0 +1,21 @@
+import subprocess
+import sys
+from pathlib import Path
+
+from kakeibo import services
+
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def dump():
+    try:
+        file = Path("~/wks/ledger/ledger_kakei/journal/kakei/main.ledger")
+        df = services.read_ledger(file)
+        for row in df.itertuples():
+            print(row)
+    except RuntimeError as e:
+        print(f"RuntimeError: {e}", file=sys.stderr)
+    except subprocess.CalledProcessError as e:
+        print(f"CalledProcessError: {e}, {e.stderr}", file=sys.stderr)
